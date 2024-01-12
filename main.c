17c5a87257c25413d076214ad2b5f6cad1e3ebad
Ivan Avdeev <me@provod.works>
2024-01-12 15:08:02 -0500

    dump formats

--- main.c
+++ main.c
@@ -56,6 +56,21 @@ static void v4l2_print_caps(uint32_t caps) {
 #undef X
 }
 
+#define MCAM_V4L2_BUF_CAPS(X) \
+	X(V4L2_BUF_CAP_SUPPORTS_MMAP) \
+	X(V4L2_BUF_CAP_SUPPORTS_USERPTR) \
+	X(V4L2_BUF_CAP_SUPPORTS_DMABUF) \
+	X(V4L2_BUF_CAP_SUPPORTS_REQUESTS) \
+	X(V4L2_BUF_CAP_SUPPORTS_ORPHANED_BUFS) \
+	X(V4L2_BUF_CAP_SUPPORTS_M2M_HOLD_CAPTURE_BUF) \
+	X(V4L2_BUF_CAP_SUPPORTS_MMAP_CACHE_HINTS) \
+
+static void v4l2_print_buf_caps(uint32_t caps) {
+#define X(cap) if (caps & cap) LOGI("  %s", #cap);
+	MCAM_V4L2_BUF_CAPS(X)
+#undef X
+}
+
 static int v4l2_query_caps(int fd) {
 	struct v4l2_capability caps;
 	if (0 != ioctl(fd, VIDIOC_QUERYCAP, &caps)) {
@@ -86,7 +101,6 @@ static const char *v4l2_input_type_name(uint32_t type) {
 		case V4L2_INPUT_TYPE_TOUCH: return "V4L2_INPUT_TYPE_TOUCH";
 		default: return "UNKNOWN";
 	}
-
 }
 
 static int v4l2_enum_input(int fd) {
@@ -94,12 +108,12 @@ static int v4l2_enum_input(int fd) {
 		struct v4l2_input input;
 		input.index = i;
 		if (0 != ioctl(fd, VIDIOC_ENUMINPUT, &input)) {
-			if (errno == EINVAL) {
+			if (EINVAL == errno) {
 				LOGI("Device has %d inputs", i);
 				return 0;
 			}
 
-			if (errno = ENOTTY) {
+			if (ENOTTY == errno) {
 				LOGI("Device has no inputs");
 				return 0;
 			}
@@ -129,19 +143,40 @@ static int v4l2_enum_controls_ext(int fd) {
 	return 0;
 }
 
-static int v4l2_enum_formats(int fd) {
+static const char *v4l2_buf_type_name(uint32_t type) {
+	switch (type) {
+		case V4L2_BUF_TYPE_VIDEO_CAPTURE: return "V4L2_BUF_TYPE_VIDEO_CAPTURE";
+		case V4L2_BUF_TYPE_VIDEO_OUTPUT: return "V4L2_BUF_TYPE_VIDEO_OUTPUT";
+		case V4L2_BUF_TYPE_VIDEO_OVERLAY: return "V4L2_BUF_TYPE_VIDEO_OVERLAY";
+		case V4L2_BUF_TYPE_VBI_CAPTURE: return "V4L2_BUF_TYPE_VBI_CAPTURE";
+		case V4L2_BUF_TYPE_VBI_OUTPUT: return "V4L2_BUF_TYPE_VBI_OUTPUT";
+		case V4L2_BUF_TYPE_SLICED_VBI_CAPTURE: return "V4L2_BUF_TYPE_SLICED_VBI_CAPTURE";
+		case V4L2_BUF_TYPE_SLICED_VBI_OUTPUT: return "V4L2_BUF_TYPE_SLICED_VBI_OUTPUT";
+		case V4L2_BUF_TYPE_VIDEO_OUTPUT_OVERLAY: return "V4L2_BUF_TYPE_VIDEO_OUTPUT_OVERLAY";
+		case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE: return "V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE";
+		case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE: return "V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE";
+		case V4L2_BUF_TYPE_SDR_CAPTURE: return "V4L2_BUF_TYPE_SDR_CAPTURE";
+		case V4L2_BUF_TYPE_SDR_OUTPUT: return "V4L2_BUF_TYPE_SDR_OUTPUT";
+		case V4L2_BUF_TYPE_META_CAPTURE: return "V4L2_BUF_TYPE_META_CAPTURE";
+		case V4L2_BUF_TYPE_META_OUTPUT: return "V4L2_BUF_TYPE_META_OUTPUT";
+		default: return "UNKNOWN";
+	}
+}
+
+static int v4l2_enum_formats(int fd, uint32_t type) {
+	LOGI("Enumerating formats for type=%s(%d)", v4l2_buf_type_name(type), type);
 	for (int i = 0;; ++i) {
 		struct v4l2_fmtdesc fmt;
 		fmt.index = i;
 		fmt.mbus_code = 0;
-		fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE; // TODO
+		fmt.type = type;
 		if (0 != ioctl(fd, VIDIOC_ENUM_FMT, &fmt)) {
-			if (errno == EINVAL) {
+			if (EINVAL == errno) {
 				LOGI("Device has %d formats", i);
 				return 0;
 			}
 
-			if (errno = ENOTTY) {
+			if (ENOTTY == errno) {
 				LOGI("Device has no formats");
 				return 0;
 			}
@@ -156,10 +191,95 @@ static int v4l2_enum_formats(int fd) {
 	}
 }
 
+static int v4l2_set_format(int fd, uint32_t type, int w, int h) {
+#if 0
+	ioctl(40, VIDIOC_G_FMT, {type=V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE, fmt.pix_mp={width=32,   height=32,   pixelformat=v4l2_fourcc('J', 'P', 'E', 'G') /* V4L2_PIX_FMT_JPEG */, field=V4L2_FIELD_NONE, colorspace=V4L2_COLORSPACE_JPEG, plane_fmt=[{sizeimage=4194304, bytesperline=0}], num_planes=1}}) = 0
+	ioctl(40, VIDIOC_S_FMT, {type=V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE, fmt.pix_mp={width=1920, height=1056, pixelformat=v4l2_fourcc('J', 'P', 'E', 'G') /* V4L2_PIX_FMT_JPEG */, field=V4L2_FIELD_ANY,  colorspace=V4L2_COLORSPACE_JPEG, plane_fmt=[{sizeimage=0, bytesperline=0}],       num_planes=1}} =>
+	                                                                 {fmt.pix_mp={width=1920, height=1056, pixelformat=v4l2_fourcc('J', 'P', 'E', 'G') /* V4L2_PIX_FMT_JPEG */, field=V4L2_FIELD_NONE, colorspace=V4L2_COLORSPACE_JPEG, plane_fmt=[{sizeimage=4194304, bytesperline=0}], num_planes=1}}) = 0
+#endif
+	int status = 0;
+	LOGI("Setting format for type=%s(%d)", v4l2_buf_type_name(type), type);
+
+	struct v4l2_format fmt;
+	fmt.type = type;
+	if (0 != ioctl(fd, VIDIOC_G_FMT, &fmt)) {
+		LOGE("Failed to ioctl(%d, VIDIOC_G_FMT): %d, %s", fd, errno, strerror(errno));
+		status = 1;
+		goto tail;
+	}
+
+#define _(v) (v)
+#define FIELDS(X) \
+	X(width, "%u", _) \
+	X(height, "%u", _) \
+	X(field, "%08x", _) \
+	X(colorspace, "%08x", _) \
+	X(num_planes, "%08x", _) \
+	X(flags, "%08x", _) \
+	X(quantization, "%08x", _) \
+	X(xfer_func, "%08x", _) \
+
+#define X(name, ffmt, func) \
+	LOGI("fmt.pix_mp." # name " = " ffmt, func(fmt.fmt.pix_mp.name));
+	FIELDS(X)
+#undef X
+#undef _
+
+	for (int i = 0; i < fmt.fmt.pix_mp.num_planes; ++i) {
+		LOGI("fmt.pix_mp.plane_fmt[%d].sizeimage = %d", i, fmt.fmt.pix_mp.plane_fmt[i].sizeimage);
+		LOGI("fmt.pix_mp.plane_fmt[%d].bytesperline = %d", i, fmt.fmt.pix_mp.plane_fmt[i].bytesperline);
+	}
+
+	fmt.fmt.pix_mp.width = w;
+	fmt.fmt.pix_mp.height = h;
+
+	if (0 != ioctl(fd, VIDIOC_S_FMT, &fmt)) {
+		LOGE("Failed to ioctl(%d, VIDIOC_S_FMT): %d, %s", fd, errno, strerror(errno));
+		status = 1;
+		goto tail;
+	}
+
+	LOGI("Set format to %dx%d", w, h);
+
+tail:
+	return status;
+}
+
+static int v4l2_requestbuffers(int fd, uint32_t type) {
+#if 0
+	ioctl(40, VIDIOC_REQBUFS, {type=V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE, memory=V4L2_MEMORY_MMAP, count=3 => 3}) = 0
+	write(2, "device/buffer_list.c: SNAPSHOT:c"..., 114) = 114
+	ioctl(40, VIDIOC_QUERYBUF_TIME32, 0x7ea46f70) = 0
+	mmap2(NULL, 4194304, PROT_READ|PROT_WRITE, MAP_SHARED, 40, 0x40000000) = 0x662a1000
+	ioctl(40, VIDIOC_EXPBUF, 0x7ea46ef4)    = 0
+	ioctl(40, VIDIOC_QUERYBUF_TIME32, 0x7ea46f70) = 0
+	mmap2(NULL, 4194304, PROT_READ|PROT_WRITE, MAP_SHARED, 40, 0x40400000) = 0x65ea1000
+	ioctl(40, VIDIOC_EXPBUF, 0x7ea46ef4)    = 0
+	ioctl(40, VIDIOC_QUERYBUF_TIME32, 0x7ea46f70) = 0
+	mmap2(NULL, 4194304, PROT_READ|PROT_WRITE, MAP_SHARED, 40, 0x40800000) = 0x65aa1000
+	ioctl(40, VIDIOC_EXPBUF, 0x7ea46ef4)    = 0
+#endif
+
+	struct v4l2_requestbuffers req = {0};
+	req.type = type;
+	req.count = 2;
+	req.memory = V4L2_MEMORY_MMAP;
+	if (0 != ioctl(fd, VIDIOC_REQBUFS, &req)) {
+		LOGE("Failed to ioctl(%d, VIDIOC_REQBUFS): %d, %s", fd, errno, strerror(errno));
+		return -1;
+	}
+
+	LOGI("req.count = %d", req.count);
+	v4l2_print_buf_caps(req.capabilities);
+	LOGI("req.flags = %08x", req.flags);
+
+	return 0;
+}
+
 static int v4l2_open(const char *devname) {
 	int status = 0;
 	LOGI("Opening %s...", devname);
-	const int fd = open(devname, O_RDWR);
+	const int fd = open(devname, O_RDWR | O_NONBLOCK);
 	if (fd < 0) {
 		LOGE("Failed to open \"%s\": %d, %s", devname, errno, strerror(errno));
 		return 1;
@@ -174,7 +294,20 @@ static int v4l2_open(const char *devname) {
 
 	v4l2_enum_input(fd);
 	v4l2_enum_controls_ext(fd);
-	v4l2_enum_formats(fd);
+	v4l2_enum_formats(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE);
+	v4l2_enum_formats(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE);
+	v4l2_enum_formats(fd, V4L2_BUF_TYPE_VIDEO_OUTPUT);
+	v4l2_enum_formats(fd, V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE);
+
+	// TODO VIDIOC_ENUM_FRAMESIZES
+	// TODO VIDIOC_ENUM_FRAMEINTERVALS
+
+	v4l2_set_format(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE, 1280, 720);
+	v4l2_requestbuffers(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE);
+
+	//v4l2_set_format(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE, 1280, 720);
+	//v4l2_requestbuffers(fd, V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE);
+
 
 cleanup:
 	close(fd);
